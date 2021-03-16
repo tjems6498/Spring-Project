0c1b290ae75da985166c84b4a928463a2519b992
tjems6498 <tjems6498@gmail.com>
2021-03-16 17:51:48 +0900

    commit

---
+++ shop-back/src/main/java/com/example/demo/shop/model/ShopOrderRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.shop.model;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShopOrderRepository extends JpaRepository<ShopOrder, Integer> {
+	@Transactional
+	@Modifying
+	@Query(value="update ShopOrder set pay=true where num=:num")
+	void updatePay(@Param("num") int num);
+}
